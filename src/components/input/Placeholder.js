f6aac2eb2ccd3ae9483b12552b647f0e06d05790
Shruti <shrutishastri49@gmail.com>
2019-08-28 17:22:34 +0530

    added responsiveness

--- src/components/input/Placeholder.js
+++ src/components/input/Placeholder.js
@@ -5,16 +5,6 @@ export const placeholder = `# Welcome to my React Markdown Previewer!
   
 Heres some code, \`<div></div>\`, between 2 backticks.
 
-\`\`\`
-// this is multi-line code:
-
-function anotherExample(firstLine, lastLine) {
-  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
-    return multiLineCode;
-  }
-}
-\`\`\`
-  
 You can also make text **bold**... whoa!
 Or _italic_.
 Or... wait for it... **_both!_**
@@ -22,13 +12,6 @@ And feel free to go crazy ~~crossing stuff out~~.
 
 There's also [links](https://www.freecodecamp.com), and
 > Block Quotes!
-
-And if you want to get really crazy, even tables:
-
-Wild Header | Crazy Header | Another Header?
------------- | ------------- | ------------- 
-Your content can | be here, and it | can be here....
-And here. | Okay. | I think we get it.
 
 - And of course there are lists.
   - Some are bulleted.
@@ -42,5 +25,4 @@ And here. | Okay. | I think we get it.
 - Even if you use dashes or asterisks.
 * And last but not least, let's not forget embedded images:
 
-![React Logo w/ Text](https://goo.gl/Umyytc)
 `;
